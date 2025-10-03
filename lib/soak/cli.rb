7a8845118b67ce7983b0a7369ff34e378d2a49d2
jefflunt <jefflunt@gmail.com>
2025-10-03 06:26:35 -0500

    Revert "basic plugin structure"
    This reverts commit b1c13258b1bab96416824545579a09fc3f6ccf70.

--- lib/soak/cli.rb
+++ lib/soak/cli.rb
@@ -1,8 +1,5 @@
 require 'thor'
 require_relative 'crawler'
-require_relative 'plugins/base'
-
-Dir[File.join(__dir__, 'plugins', '*.rb')].each { |file| require file }
 
 module Soak
   class CLI < Thor
@@ -11,16 +8,5 @@ module Soak
       crawler = Crawler.new(url, depth.to_i)
       crawler.crawl
     end
-
-    desc "plugin NAME [ARGS...]", "run a plugin"
-    def plugin(name, *args)
-      plugin_class = Soak::Plugins::Base.plugins[name]
-      if plugin_class
-        plugin_class.new.run(args)
-      else
-        puts "plugin '#{name}' not found"
-      end
-    end
   end
 end
-
